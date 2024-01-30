5d4c12685c91f39ec5ca65bf9ae24de3009f3898
Laurent <laurent.roches.simplon@gmail.com>
2024-01-30 09:41:04 +0100

    Exercice complete, drag & drop reussit!

--- script/script.js
+++ script/script.js
@@ -1,16 +1,31 @@
 let firstSquare = document.getElementById('playground');
 let secondSquare = document.getElementById('square');
 let thirdSquare = document.getElementById('square2');
+let isDragging = false;
 
-firstSquare.addEventListener('click', (e)=>{
-    console.log(`Client X/Y: ${e.clientX}, ${e.clientY}`);
+firstSquare.addEventListener('mousedown', (e) => {
+    isDragging = true;
+    thirdSquare.style.top=`${e.clientY-50}px`;
+    thirdSquare.style.left=`${e.clientX-60}px`;
+    let drag = (e) => {
+        if (isDragging) {
+            thirdSquare.style.left = `${e.clientX - 60}px`;
+            thirdSquare.style.top = `${e.clientY - 50}px`;
+        };
+    };
+    let noDrag = () => {
+        isDragging = false;
+        document.removeEventListener('mousemove', drag);
+        document.removeEventListener('mouseup', noDrag);
+    };
+    document.addEventListener('mousemove', drag);
+    document.addEventListener('mouseup', noDrag);
+});
+firstSquare.addEventListener('mouseup', (e) => {
     secondSquare.style.top = `${e.clientY-50}px`;
     secondSquare.style.left = `${e.clientX-60}px`;
 });
 
-document.onmousemove = function (e){
-    thirdSquare.style.top=`${e.clientY-50}px`;
-    thirdSquare.style.left=`${e.clientX-60}px`;
+firstSquare.ondragstart = function () {
+    return false;
 };
-
-
